34d7ba5c566670ebe8892002232b68639748f834
Cbih939 <memegold999@gmail.com>
2025-08-26 14:43:57 -0300

    FIX: login

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
-// Cria uma instância do Axios
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'https://selocidadania.redepapelsolidario.org.br/api',
 });
 
-// Interceptor para adicionar o token em todas as requisições
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token'); // pega o token do storage
+    const token = localStorage.getItem('token');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
