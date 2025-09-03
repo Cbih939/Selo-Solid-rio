b9c62ba1b76fbdb84461364379828bf6fec1bf43
Cbih939 <memegold999@gmail.com>
2025-09-03 16:55:38 -0300

    Atualização routes e controller

--- selo-cidadania-backend/routes/reportsRoutes.js
+++ selo-cidadania-backend/routes/reportsRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const reportsController = require('../controllers/reportsController');
 
+router.get('/', reportsController.getReports);
 router.get('/stats', reportsController.getSystemStats);
 router.get('/redemptions', reportsController.getAllRedemptions);
 router.get('/ong/:ongId', reportsController.getOngStats);
 router.get('/', reportsController.getReportsData);
-router.get('/', reportsController.getReports);
 
 module.exports = router;
