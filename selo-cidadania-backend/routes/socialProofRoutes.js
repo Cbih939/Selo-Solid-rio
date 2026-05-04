021b472b746a45a3e84776e07dc0d2811b411671
Cbih939 <memegold999@gmail.com>
2026-05-04 11:48:05 -0300

    atualização no socialproofroutes

--- selo-cidadania-backend/routes/socialProofRoutes.js
+++ selo-cidadania-backend/routes/socialProofRoutes.js
@@ -1,8 +1,11 @@
+// Arquivo: selo-cidadania-backend/routes/socialProofRoutes.js
+
 const express = require('express');
 const router = express.Router();
 const socialProofController = require('../controllers/socialProofController');
 const upload = require('../middlewares/upload');
 
+// Rotas existentes
 router.get('/ongs-list', socialProofController.getAllOngs);
 router.get('/activities/ong/:ongId', socialProofController.getActivitiesByOng);
 router.post('/activities', upload.single('activity_image'), socialProofController.createActivity);
@@ -12,13 +15,28 @@ router.delete('/activities/:id', socialProofController.deleteActivity);
 router.post('/', upload.array('proof_files', 5), socialProofController.createSocialProof);
 router.get('/user/:userId', socialProofController.getUserProofs);
 router.get('/pending/:ongId', socialProofController.getPendingProofs);
-// ++ NOVA ROTA DO LOG DE AUDITORIA ++
+
+// Rota do log de auditoria
 router.get('/log/:ongId', socialProofController.getEvaluationLog); 
 
 router.put('/:proofId/approve', socialProofController.approveProof);
 router.put('/:proofId/reject', socialProofController.rejectProof);
 router.put('/:proofId/message', socialProofController.sendMessage);
 router.put('/:proofId/resubmit', socialProofController.requestResubmission);
-router.get('/activities', verifyToken, socialProofController.getActivitiesList);
+
+// =========================================================================
+// ROTA DE CATÁLOGO DE ATIVIDADES COM AUTENTICAÇÃO INTELIGENTE
+// =========================================================================
+const authMiddleware = require('../middlewares/authMiddleware');
+let activeAuth = (req, res, next) => next();
+
+if (typeof authMiddleware === 'function') {
+    activeAuth = authMiddleware;
+} else if (authMiddleware !== null && typeof authMiddleware === 'object') {
+    const exportedFunctions = Object.values(authMiddleware).filter(val => typeof val === 'function');
+    if (exportedFunctions.length > 0) activeAuth = exportedFunctions[0];
+}
+
+router.get('/activities', activeAuth, socialProofController.getActivitiesList);
 
 module.exports = router;
